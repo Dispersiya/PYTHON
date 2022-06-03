c9440b1c378c2aa5cf540a6c9d6a01cab95b1026
Vika <nika-maks@inbox.ru>
2022-06-03 15:21:58 +0300

    дз задача 3.2, 3.3

--- homework3.1-3.5.py
+++ homework3.1-3.5.py
@@ -26,4 +26,32 @@ num1 = int(input("Введите 1 число: "))
 num2 = int(input("Введите 2 число: ")) 
 print("НОК", num1,"и", num2,"=", calculate_lcm(num1, num2))
 """
-#2
+#2 Вычислить число Пи c заданной точностью d
+# Пример: при d = 0.001,  c= 3.141. 
+'''
+import math
+d = "0.001"
+d = int(len(d) - 2)
+pi = round(math.pi, d)
+print(pi)
+print('{:.20}'.format(math.pi))
+'''
+#3.Составить список простых множителей натурального числа N
+import os
+os.system("cls")
+
+n = int(input('Введите число: '))
+def prime_number (n):
+    i = 2
+    array = []
+    while n != 1: 
+        if n % i == 0:
+            array.append(i) 
+            n = n / i
+            i = 2
+        else: 
+            i += 1
+    return (array)
+array = prime_number(n)
+print(f'Список простых множителей натурального числа {n} = {array}')
+
